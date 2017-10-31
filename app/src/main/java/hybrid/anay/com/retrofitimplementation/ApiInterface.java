f0ea058be9e3ee3c54f7cbd6ae0b014be288be3c
unknown <neha.8xx@gmail.com>
2017-10-31 18:31:12 +0530

    first commit

---
+++ app/src/main/java/hybrid/anay/com/retrofitimplementation/ApiInterface.java
@@ -0,0 +1,18 @@
+package hybrid.anay.com.retrofitimplementation;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+/**
+ * Created by Anay on 10/31/2017.
+ */
+
+public  interface ApiInterface {
+    @GET("movie/top_rated")
+     Call<MoviesResponse> getTopRatedMovies(@Query("api_key") String apiKey);
+
+    @GET("movie/{id}")
+     Call<MoviesResponse> getMovieDetails(@Path("id") int id, @Query("api_key") String apiKey);
+}
